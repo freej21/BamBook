8046b6ade58d77e780140453b5a0000f49f3b9b2
Neon Vaughn Vesper <neonvaughnm@gmail.com>
2023-12-11 23:12:21 +0800

    initial commit

--- user/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(label='Username', max_length=150)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
